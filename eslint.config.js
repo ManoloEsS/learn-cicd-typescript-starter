732e113490640ec037ac17e4a8be297b97d08569
ManoloEsS <manolo.estrada.s@gmail.com>
2026-02-20 20:41:00 -0700

    fix user generated id to use randomBytes

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ export default defineConfig([
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
         plugins: { js },
         extends: ["js/recommended"],
+        ignores: ["**/dist/**", "**/coverage/**"],
     },
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
